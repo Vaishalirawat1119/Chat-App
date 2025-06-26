11553295992f7154068a3e9e13e6b6d1403bd8f0
Vaishali <rawatvaishali254@gmail.com>
2025-06-26 22:19:33 +0530

    Updated App.jsx and removed Home.jsx

--- chatFront/src/App.jsx
+++ chatFront/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Home from './components/Home'
+import Home from './pages/Home'
 
 function App() {
 
