61c282de5f1c742ddcf3292ce4e4de14107d7261
Vaishali <rawatvaishali254@gmail.com>
2025-06-30 22:09:22 +0530

    feat: implemented Axios POST request to register user via backend API and added password column to the register page

--- chatFront/src/components/Register.jsx
+++ chatFront/src/components/Register.jsx
@@ -1,13 +1,16 @@
 import React, { useState } from 'react'
+import axios from 'axios'
 
 const Register = ({openLogin}) => {
 
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [file, setFile] = useState(null);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    axios.post('http://localhost:2000/chat/user/register', {name, email, password})
   }
 
   return (
@@ -21,6 +24,10 @@ const Register = ({openLogin}) => {
         <div className='mb-4'>
           <label className='block text-gray-700'>Email:</label>
           <input type="email" className='w-full px-3 py-2 border' onChange={(e) => setEmail(e.target.value)} placeholder='Enter Email'/>
+        </div>
+        <div className='mb-4'>
+          <label className='block text-gray-700'>Password:</label>
+          <input type="password" className='w-full px-3 py-2 border' onChange={(e) => setPassword(e.target.value)} placeholder='Enter Password'/>
         </div>
         <div className='mb-4'>
           <label className='block text-gray-700'>Upload Image</label>
